42bcb448bf6fd1a2538e53170a696f844a0ec566
drmrhdt <drmrhdt@gmail.com>
2022-01-16 02:03:35 +0300

    fix: fix carousel

--- apps/tech-shop/src/app/main/recommendations/recommendations.component.ts
+++ apps/tech-shop/src/app/main/recommendations/recommendations.component.ts
@@ -14,6 +14,7 @@ export class RecommendationsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getRandomSuggestions();
+    this.getCategories();
   }
 
   async getRandomSuggestions(): Promise<void> {
