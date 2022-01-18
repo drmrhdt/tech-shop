4c76decb7dc7129c3bbf03b05eb40f6817d08e0d
drmrhdt <drmrhdt@gmail.com>
2022-01-18 12:58:30 +0300

    refactor: replace any with Product interface

--- apps/tech-shop/src/app/products/product-list/product-list.component.ts
+++ apps/tech-shop/src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Product } from '../../../models';
 
 @UntilDestroy({ checkProperties: true })
 @Component({
@@ -9,7 +10,7 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-  products: any[] = [];
+  products: Product[] = [];
 
   constructor(private _route: ActivatedRoute) {}
 
