e0beda59040bef61362c2543ff711b30c29b83c7
drmrhdt <drmrhdt@gmail.com>
2022-01-24 00:12:30 +0300

    feat: add rating and style card in the product list

--- apps/tech-shop/src/app/products/product-list/product-list.module.ts
+++ apps/tech-shop/src/app/products/product-list/product-list.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 import { ProductListComponent } from './product-list.component';
 import { WrapperModule } from '../../shared/wrapper/wrapper.module';
 import { MenuModule } from '../../shared/menu/menu.module';
+import { RatingModule } from '../../shared/rating/rating.module';
 
 @NgModule({
   declarations: [ProductListComponent],
@@ -18,6 +20,8 @@ import { MenuModule } from '../../shared/menu/menu.module';
     MenuModule,
     NzGridModule,
     WrapperModule,
+    RatingModule,
+    NzButtonModule,
   ],
   exports: [ProductListComponent],
 })
